84a79989b795bff1f662fbb84124d777aab94eb7
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-08 18:49:57 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
