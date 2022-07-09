d36863dc8f33712d25c07d267dd1c6cb315a8089
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-08 21:19:28 -0400

    contact

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,20 @@ import React from 'react';
 function About() {
     return (
       <section className="my-5">
-        <h1 id="about">Placeholder</h1>
+        
         <h2 class="myname">Roger Chan</h2>
-        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-        <div className="my-2">
+        <img src={screenshotofmyself.png} class="my-2" style={{ width: "100%" }} alt="Roger Chan" />
+        <div>
           <p>
-            Lorem ipsum dolor 
+            Ohio State University coding bootcamp student graduating in 07/2022
           </p>
+          <p>
+          Full-Stack web developer with developed coding skills (HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, MongoDB, MySQL, Command Line, and Git)
+          </p>
+          <p>
+            A self-motivated, organized, detail-oriented and creative quick learner
+          </p>
+
         </div>
       </section>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,36 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Navigation from "../../components/Navigation";
 import About from "../../components/About";
 import Portfolio from "../../components/Portfolio";
 import Contact from '../../components/Contact';
 import Resume from '../../components/Resume';
-import { HashRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
 
-function Header() {
+const routes = {
+    "/user": () => <Users />,
+    "/about": () => <About />,
+    "/contact": () => <Contact />
+  };
+  
+  function App() {
+    const routeResult = useRoutes(routes);
     return (
-        <Router>
-            <Navigation/>
-            <Switch>
-                <Route exact path="/">
-                    <Redirect to="/About" />
-                </Route>
-                <Route path="/About">
-                    <About />
-                </Route>
-                <Route path="/Portfolio" component={Portfolio} />
-                <Route path="/Contact" component={Contact} />
-                <Route path="/Resume">
-                    <Resume />
-                </Route>
-            </Switch>
-        </Router>
-    )
-}
+      <div className="App">
+        <A href="/user">Users Page</A>
+        <A href="/about">About Page</A>
+        <A href="/contact">Contacts Page</A>
+        {routeResult}
+      </div>
+    );
+  }
+  
+const routes = {
+    "/user": () => <Users />,
+    "/about": () => <About />,
+    "/contact": () => <Contact />
+  };
+
+
 
 export default Header;
